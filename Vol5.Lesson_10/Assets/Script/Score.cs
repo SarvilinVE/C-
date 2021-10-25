af71c5e3b3db85104cc93bd69b8dda421ba2c033
SarvilinVE <sarvili.ve@yandex.ru>
2021-10-25 18:38:34 +0500

    Домашняя работа 10. Реализован только игрок, но при поедании очки начисляются

---
+++ Vol5.Lesson_10/Assets/Script/Score.cs
@@ -0,0 +1,13 @@
+﻿namespace MVVM.Model
+{
+    internal sealed class Score : IScore
+    {
+        public float MaxScore { get; }
+        public float CurrentScore { get; set; }
+        public Score(float maxScore)
+        {
+            MaxScore = maxScore;
+            CurrentScore = MaxScore;
+        }
+    }
+}
